e0524b0251a898431a388cce77d94402462e8fe1
Alastair Smith <github@alastairsmith.me.uk>
2012-03-01 23:04:09 +0000

    Naive parallel solution (currently broken).
    Some threads never get to the base case, meaning that the tests hang.
    This indicates an error in the Split() implementation.

--- ParallelDesignPatterns/ParallelDesignPatterns/MinValueFinder.cs
+++ ParallelDesignPatterns/ParallelDesignPatterns/MinValueFinder.cs
@@ -3,19 +3,74 @@ using System.Linq;
 
 namespace ParallelDesignPatterns
 {
-    class MinValueFinder
+    class MinValueFinder : AbstractDividAndConquer<Problem, int>
     {
-        private readonly Func<int, int> f;
+        private readonly Func<int, int> function;
 
-        public MinValueFinder(Func<int, int> f)
+        public MinValueFinder(Func<int, int> function)
         {
-            this.f = f;
+            this.function = function;
         }
 
         internal int FindMinimum(int a, int b)
         {
-            int numberOfInputs = b - a + 1;
-            return Enumerable.Range(a, numberOfInputs).Select(f).Min();
+            return Solve(new Problem(function, a, b));
         }
+
+        protected override Problem[] Split(Problem problem)
+        {
+            int rangeOfInput = problem.UpperBound - problem.LowerBound;
+            int boundary;
+            if (rangeOfInput % 2 == 0)
+            {
+                boundary = rangeOfInput / 2;
+            }
+            else
+            {
+                // Handle the truncation.
+                boundary = (rangeOfInput / 2) + 1;
+            }
+
+            return new[]
+                       {
+                           new Problem(function, problem.LowerBound, problem.LowerBound + boundary-1),
+                           new Problem(function, problem.UpperBound - boundary, problem.UpperBound)
+                       };
+        }
+
+        protected override bool IsBaseCase(Problem problem)
+        {
+            // Naive implementation creates a new thread for every division of the list
+            // until the list can be divided no more.
+            return problem.UpperBound == problem.LowerBound;
+        }
+
+        protected override int BaseCaseSolve(Problem problem)
+        {
+            // Doesn't matter whether we return LowerBound or UpperBound here, as they
+            // should be equal.
+            return problem.LowerBound;
+        }
+
+        protected override int Merge(int[] subSolutions)
+        {
+            return subSolutions.Min();
+        }
+    }
+
+    class Problem
+    {
+        public Problem(Func<int, int> function, int lowerBound, int upperBound)
+        {
+            Function = function;
+            LowerBound = lowerBound;
+            UpperBound = upperBound;
+        }
+
+        protected Func<int, int> Function { get; private set; }
+
+        public int LowerBound { get; private set; }
+
+        public int UpperBound { get; private set; }
     }
 }
